3106b62dd83fe3d8a9ccad896f9f6ffe13aea9a0
zaknafein83 <franksisca@gmail.com>
2021-12-14 10:24:22 +0100

    first commit

---
+++ login-example/src/main/java/formazione/servlet/ControllerAzione.java
@@ -0,0 +1,24 @@
+package formazione.servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/azione")
+public class ControllerAzione extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		
+		System.out.println(username +" - " + password);
+		
+getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
+	}
+
+}
---
+++ login-example/src/main/webapp/index.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+<form action="azione" method="post">
+
+	<label for="name"> Inserisci username</label><br>
+	<input type="text" name="username" placeholder="Inserisci username"><br>
+	
+	<label for="surname"> Inserisci password</label><br>
+	<input type="password" name="password" placeholder="Inserisci password"><br>
+	<br>
+	
+  	<input type="submit" value="Clicca qui">
+  	
+</form>
+</body>
+</html>
